9acb1d4f655b80aa7d4acd967598b8c63756fe70
darshan-Code11 <darshugowda0581@gmail.com>
2026-03-10 22:52:05 +0530

    Add proxy: true to GoogleStrategy to fix HTTPS redirects on Render

--- server.js
+++ server.js
@@ -87,7 +87,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         await User.findOneAndUpdate(
